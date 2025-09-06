6ddf2d9a0931512d6ca1b2bcac12c948b43bdea0
Nikita Shchipkov <nikitahello123@mail.ru>
2025-09-06 13:44:57 +0300

    task 4

---
+++ l1.4/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				fmt.Println("working")
+				time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+	cancel()
+	fmt.Println("done")
+}
